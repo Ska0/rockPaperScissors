e8123e1bc3bc72c91da6c20a2136f3b8dba5ce8c
Jonathan Towery <misfit.for.fame@gmail.com>
2021-12-05 02:18:46 -0600

    added for loop

--- script.js
+++ script.js
@@ -1,52 +1,64 @@
-
 const weaponOfChoice = ["rock", "paper", "scissors"];
-let roundCount = 0;
-let score = 0;
+let playerScore = 0;
+let computerScore = 0;
 function computerPlay() {
-      
-         return weaponOfChoice[Math.floor(Math.random()*weaponOfChoice.length)];
-   };
+  return weaponOfChoice[Math.floor(Math.random() * weaponOfChoice.length)];
+}
 
-   function playerPlay() {
-     let pick = prompt("rock, paper, or scissors?")
-      return pick;
-   }
+function playerPlay() {
+  let pick = prompt("rock, paper, or scissors?");
+  pick = pick.toLowerCase(length);
+  return pick;
+}
 
-  function round() {
-     let playerPick = playerPlay();
-     let computerPick = computerPlay();
-     console.log("round " + roundCount);
-     console.log("your score is " + score);
-     if (playerPick === computerPick) {
-        console.log("You Picked: " + playerPick);
-        console.log("The Computer Picked: " + computerPick);
-        console.log("It's a tie! choose again!");
-        return round();
-     } else if (playerPick === "rock" && computerPick != "paper") {
-        console.log("You Picked: " + playerPick);
-        console.log("The Computer Picked: " + computerPick);
-        console.log("you win! your score went up by 1!");
-        roundCount++; 
-         score++;
-         return round();
-     } else if (playerPick === "paper" && computerPick != "scissors") {
-      console.log("You Picked: " + playerPick);
-      console.log("The Computer Picked: " + computerPick);
-      console.log("you win! your score went up by 1!");
-      roundCount++;
-       score++;
-       return round();
-     } else if (playerPick === "scissors" && computerPick != "rock") {
-        console.log("You Picked: " + playerPick);
-        console.log("The Computer Picked: " + computerPick);
-        console.log("you win! your score went up by 1!");
-        roundCount++;
-        score++;
-        return round();
-     } else {
-      console.log("You Picked: " + playerPick);
-      console.log("The Computer Picked: " + computerPick);
-      console.log("you lose!")
-      return round();
-     };
-  };
+function game() {
+  for (let i = 0; i < 5; i++) {
+    round();
+  }
+  if (playerScore > computerScore) {
+    return alert(
+      "You Win! Final score - Player: " +
+        playerScore +
+        " Computer: " +
+        computerScore
+    );
+  } else if (playerScore < computerScore) {
+    return alert(
+      "You Lose! Final score - Player: " +
+        playerScore +
+        " Computer: " +
+        computerScore
+    );
+  }
+}
+
+function round(playerPick, computerPick) {
+  playerPick = playerPlay();
+  computerPick = computerPlay();
+  if (playerPick === computerPick) {
+    console.log("You Picked: " + playerPick);
+    console.log("The Computer Picked: " + computerPick);
+    console.log("It's a tie! choose again!");
+    playerPick = playerPlay();
+  } else if (playerPick === "rock" && computerPick != "paper") {
+    console.log("You Picked: " + playerPick);
+    console.log("The Computer Picked: " + computerPick);
+    console.log("you win! your score went up by 1!");
+    playerScore++;
+  } else if (playerPick === "paper" && computerPick != "scissors") {
+    console.log("You Picked: " + playerPick);
+    console.log("The Computer Picked: " + computerPick);
+    console.log("you win! your score went up by 1!");
+    playerScore++;
+  } else if (playerPick === "scissors" && computerPick != "rock") {
+    console.log("You Picked: " + playerPick);
+    console.log("The Computer Picked: " + computerPick);
+    console.log("you win! your score went up by 1!");
+    playerScore++;
+  } else {
+    console.log("You Picked: " + playerPick);
+    console.log("The Computer Picked: " + computerPick);
+    console.log("you lose! " + computerPick + "beats " + playerPick);
+    computerScore++;
+  }
+}
